5393dced6172f455c1fe299221b68811244db164
Icarucci <ignaciocarucci@hotmail.com>
2024-06-06 22:52:09 -0300

    SegundaEntrega

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let log1 = "ADMIN";
 let log2 = "ADMIN";
 let saldo = 0.0;
+const arrayContacto = [];
 
 
 inicio();
@@ -47,7 +48,7 @@ function crearUsuario() {
         }
 
 function operaciones(){
-        let operacion = prompt ("¿Que desea realizar?: " + "//" + "Consulta de Saldo (1) " + "//" + "Recargar Saldo (2)" + "//" + "Pagar Servicios (3)" + "//" + "Salir (4)");
+        let operacion = prompt ("Hola, " + log1 + ".\n \n¿Que deseas realizar hoy?:\n \n(1) Consulta de Saldo\n(2) Recargar Saldo\n(3) Transferir dinero\n(4) Pagar Servicios\n(5) Salir");
         if (operacion === "1"){
             consultaDeSaldo();
         }
@@ -55,9 +56,12 @@ function operaciones(){
             recargaSaldo();
         }
         else if (operacion === "3"){
-            pagarServicios();
+            transferirDinero();
         }
         else if (operacion === "4"){
+            pagarServicios();
+        }
+        else if (operacion === "5"){
             alert("Muchas gracias por utilizar PagoMisDeudas. ¡Hasta luego!");
             inicio();
         }
@@ -99,6 +103,41 @@ function recargaSaldo(){
             }
         }
 
+function transferirDinero(){
+            let transferir = prompt("Transferir dinero\n \n(1) Elegir contacto por lista\n \n(2) Buscar Contacto por tel\n \n(3) Agregar contacto\n \n(4) Volver atrás");
+            if (transferir === "1"){
+                elegirContacto();
+            }
+            else if(transferir === "2"){
+                let buscador = prompt("Ingrese numero del contacto");
+                const telefono = arrayContacto.find(contacto => contacto.telefono === buscador);
+                if (telefono == undefined){
+                    alert("El contacto no existe.");
+                    transferirDinero();
+                }
+                else {
+                    alert("Contacto encontrado: " + telefono.apodo)
+                    transferirDinero();
+                }
+            }
+            else if (transferir === "3"){
+                //Constructores
+                agregarContacto();
+
+                // Instancia del objeto persona
+                const persona1 = new agregarContacto(
+                    prompt("Ingrese el apodo"),
+                    prompt("Ingrese el nombre completo"),
+                    prompt("Ingrese el Telefono"));
+                alert("Nuevo contacto Agregado!\n \n" + persona1.apodo + "\n" + persona1.telefono);
+                arrayContacto.push(persona1);
+                transferirDinero();
+            }
+            else{
+                operaciones();
+            }
+}
+
 function pagarServicios(){
             let servicios = prompt("¿Cuantos servicios desea pagar?");
             if (servicios > 0){
@@ -133,10 +172,53 @@ function pagarServicios(){
                 alert("Valor incorrecto.")
                 pagarServicios();
             }
-            
+}
 
-
-
+function elegirContacto(){
+    let lista = "";
+    for (let i = 0; i < arrayContacto.length; i++) {
+        lista += ((i+1) + ". " + arrayContacto[i].apodo + "\n");
+    }
+    let contacto = prompt("¿A quién le querés transferir?\n" + "\n" + lista + " \n" + "\n" + "(V) Volver atrás");
+    if (contacto >= 1 && contacto <= arrayContacto.length){ 
+        transferirAPersona(contacto - 1);
+        transferirDinero();
+    }
+    else if (contacto === "v"){
+        transferirDinero();
+    }
+    else{
+        alert("Opcion incorrecta.")
+        elegirContacto();
+    }
 }
 
 
+
+//Constructor
+function agregarContacto(apodo, nombre, telefono) {
+    this.apodo = apodo;
+    this.nombre = nombre;
+    this.telefono = telefono;
+
+}
+
+function transferirAPersona(index){
+    let valor3 = prompt("Ingrese el monto que desea transferir a: " +  arrayContacto[index].apodo);
+    alert("Vas a transferir: $ " + valor3);
+    valor3 = parseFloat(valor3);
+        if (valor3 > 0 && valor3 < saldo){
+            saldo-=valor3;
+            alert("Transacción realizada con exito.");
+            alert("Saldo actual: $" + saldo);
+        }
+        else if(valor3 < 0){
+            alert("El importe debe ser positivo.");
+        }
+        else if(valor3 > 0 && valor3 > saldo){
+            alert("Saldo insuficiente.");
+        }
+        else{
+            alert("Valor incorrecto.");
+        }
+    }
